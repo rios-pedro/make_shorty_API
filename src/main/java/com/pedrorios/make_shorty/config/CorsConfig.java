b2b75698ac650ab6092d06737d28743cbfa66d6e
rios-pedro <pedro.henrique9760@icloud.com>
2026-08-03 22:18:53 -0300

    fix: double url

--- src/main/java/com/pedrorios/make_shorty/config/CorsConfig.java
+++ src/main/java/com/pedrorios/make_shorty/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://make-shorty.vercel.app/")
+                        .allowedOrigins("https://make-shorty.vercel.app", "https://make-shorty.vercel.app/")
                         .allowedMethods("GET", "POST");
             }
         };
